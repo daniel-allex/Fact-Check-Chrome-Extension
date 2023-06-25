d67ba2af0e6b69e149300ddf75f34e982494478e
Daniel Allex <danny.allex@gmail.com>
2023-06-25 17:36:35 -0400

    Reformatted Structure

--- scripts/add_to_db.py
+++ scripts/add_to_db.py
@@ -6,9 +6,5 @@ with open('data_embeddings.json') as f:
     # Load JSON data from file
     data = json.load(f)
 
-#for i in range(len(data)):
-    #data[key]
-
-
 db = database.Database("mongodb://localhost:27017/", "FakeNewsDB", "PoliticalData")
 db.insert_many(data)
